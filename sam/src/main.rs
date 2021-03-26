c2b199e4b257fecc2d57cabd25f72ce538c4a648
ashtneoi <ashtneoi@gmail.com>
2021-03-26 13:17:36 -0700

    Do lui

--- sam/src/main.rs
+++ sam/src/main.rs
@@ -1,3 +1,113 @@
+use std::collections::HashMap;
+use std::io::{self, prelude::*};
+
+#[derive(Clone, Copy)]
+enum InsnType {
+    R,
+    I,
+    S,
+    B,
+    U,
+    J,
+    Sxli,
+}
+
+fn parse_reg(s: &str) -> Result<u32, String> {
+    if !s.starts_with('x') {
+        return Err("invalid register".to_string());
+    }
+    let s = &s["x".len()..];
+    let mut n: u32 = 0;
+    for c in s.chars() {
+        if n >= 32 {
+            return Err("number is larger than 5 bits".to_string());
+        }
+        n <<= 10;
+        n += match c {
+            '0'..='9' => c as u32 - '0' as u32,
+            _ => return Err(format!("invalid decimal digit '{}'", c)),
+        };
+    }
+    Ok(n)
+}
+
+fn from_hex(s: &str, width: u32) -> Result<u32, String> {
+    let is_neg = s.starts_with('-');
+    let s = if is_neg { &s["-".len()..] } else { s };
+    if !s.starts_with('#') {
+        return Err("number doesn't start with '#'".to_string());
+    }
+    let s = &s["#".len()..];
+    if s.len() == 0 {
+        return Err("number is empty".to_string());
+    }
+    let mut n: u32 = 0;
+    for c in s.chars() {
+        if let '\'' | '_' = c {
+            continue;
+        }
+        if n >= 1 << (width-4) {
+            return Err(format!("number is larger than {} bits", width));
+        }
+        n <<= 4;
+        n += match c {
+            '0'..='9' => c as u32 - '0' as u32,
+            'A'..='F' => c as u32 - 'A' as u32 + 10,
+            'a'..='f' => c as u32 - 'a' as u32 + 10,
+            _ => return Err(format!("invalid hex digit '{}'", c)),
+        };
+    }
+    if is_neg {
+        if n >= 1 << (width-1) {
+            return Err("number is too large to negate".to_string());
+        }
+        n = (!n).wrapping_add(1);
+    }
+    Ok(n)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut labels = HashMap::new();
+
+    let mut mnemonics = HashMap::new();
+    mnemonics.insert("lui", (InsnType::U, 0x0000_0037));
+    let mnemonics = mnemonics;
+
+    let mut addr: u32 = 0x8000_0000;
+    let stdin_unlocked = io::stdin();
+    let stdin = stdin_unlocked.lock();
+    for line in stdin.lines() {
+        let line = line.unwrap();
+        if line.starts_with('$') {
+            // label
+            let label = line["$".len()..].to_string();
+            labels.insert(label, addr);
+        } else if line.chars().all(|c| c.is_ascii_whitespace()) {
+            // nothing
+        } else {
+            // instruction
+            let mut parts = line.split_ascii_whitespace();
+            let mnemonic = parts.next().expect("missing mnemonic");
+            let (insn_type, template) = mnemonics[&mnemonic];
+            let mut insn = template;
+            match insn_type {
+                InsnType::U => {
+                    let rd = parts.next().expect("missing rd");
+                    let rd = parse_reg(rd).unwrap();
+                    let imm = parts.next().expect("missing imm20");
+                    let imm = from_hex(imm, 20).unwrap();
+                    insn += (rd << 7) + (imm << 12);
+                },
+                _ => todo!(),
+            }
+            addr += 4;
+            println!(
+                "{:04X}'{:04X}: {:04X}'{:04X}",
+                addr >> 16,
+                addr & 0xFFFF,
+                insn >> 16,
+                insn & 0xFFFF,
+            );
+        }
+    }
 }
