d126de8a082d9c6c00824dcf9a511573bf60cd86
ashtneoi <ashtneoi@gmail.com>
2021-04-11 11:01:44 -0700

    Remove unnecessary fences
    According to the RISC-V Unix Platform Profile, I/O regions are
    strongly-ordered (see section A.4 of the spec for what that means).

--- sam/echo.s
+++ sam/echo.s
@@ -1,20 +1,16 @@
 $read
             lui x1 #1000'0
             addi x2 x0 #01
-            fence iorw iorw
             lb x4 x1 #5
             and x4 x4 x2
             beq x4 x0 -#8
-            fence iorw iorw
             lb x3 x1 #0
 
 $write
             addi x2 x0 #20
-            fence iorw iorw
             lb x4 x1 #5
             and x4 x4 x2
             beq x4 x0 -#8
-            fence iorw iorw
             sb x3 x1 #0
             jal x0 read
 
